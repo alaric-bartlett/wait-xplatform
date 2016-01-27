d17da0de56318fd60a7f119da7b208244be07ce9
alaric-bartlett <alaric.bartlett15@pcc.edu>
2016-01-27 10:57:54 -0800

    Update kbhit.cpp

--- kbhit.cpp
+++ kbhit.cpp
@@ -22,6 +22,13 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
+/*
+ * Function: changemode
+ * Purpose: Switch off(or on) the icanon flag for the terminal 
+ *          Basically, tells the terminal whether or not to print the key to the output stream after it is pressed.
+ * Return type: void
+ * Arguments: int dir: 1 or 0 to indicate operation
+ */
 void changemode(int dir)
 {
 	static struct termios oldt, newt;
@@ -37,6 +44,12 @@ void changemode(int dir)
 		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
+/*
+ * Function: _kbhit()
+ * Purpose: Read state of keyboard
+ * Return type: int, 1 or 0 depending on whether a key has been pressed
+ * Arguments: None
+ */
 int _kbhit(void)
 {
 	struct timeval tv;
@@ -55,6 +68,12 @@ int _kbhit(void)
 
 #endif
 
+/*
+ * Function: Wait()
+ * Purpose: Wait for a keypress, then return keypress to user
+ * Return type: Int
+ * Arguments: None
+ */
 int Wait()
 {
 	int c;
